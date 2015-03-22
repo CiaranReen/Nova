addb942a4d821a0c5dea6b33a8962bf1bbe476e8
Ciaran <ciaran@creativeemporium.co.uk>
2015-03-22 00:40:27 +0000

    dded in base controller for app-wide functionality

---
+++ app/controllers/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * ------------------------------------------------------
+ *  App-wide controller functionality
+ * ------------------------------------------------------
+ */
+
+class BaseController extends Controllers_Base {
+
+    public function __construct() {
+        parent::__construct();
+    }
+}
